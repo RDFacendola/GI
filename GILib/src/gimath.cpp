acbfa6b49adb00135432baa164a2f64006ecefc6
Raffaele Daniele Facendola <raf.dan.fac@gmail.com>
2014-10-01 23:28:41 +0200

    Ops, forgot about this three commits ago ;)

---
+++ GILib/src/gimath.cpp
@@ -0,0 +1,11 @@
+#include "..\include\gimath.h"
+
+#include <math.h>
+
+using namespace gi_lib;
+
+float Math::kPi = 3.1415926f;
+
+float Math::kDegToRad = Math::kPi / 180.0f;
+
+float Math::kRadToDeg = 1.0f / Math::kDegToRad;
