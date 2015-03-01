8a53b0f839f220545877b17f6cc24828f2f23b74
Raffaele Daniele Facendola <raf.dan.fac@gmail.com>
2015-03-01 23:00:07 +0100

    Ignoring doc stuffs...

--- GILib/doc/html/search/defines_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['return_5fon_5ffail',['RETURN_ON_FAIL',['../exceptions_8h.html#ac98287e45885f8d838a1726eec3c4ca5',1,'exceptions.h']]]
-];
--- GILib/doc/html/search/defines_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['throw_5fon_5ferror',['THROW_ON_ERROR',['../exceptions_8h.html#a327e270316af41b131296b3723a03b4f',1,'exceptions.h']]],
-  ['throw_5fon_5ffail',['THROW_ON_FAIL',['../exceptions_8h.html#a5c7aeefc3de8b2dc5089a61381611c9b',1,'exceptions.h']]],
-  ['throw_5fon_5fzero',['THROW_ON_ZERO',['../exceptions_8h.html#aa7e6eca6f530ed8c984cc853cb078a9c',1,'exceptions.h']]]
-];
