7d14bf558c1c94d5b31324e469426750b45a4cc7
Raffaele Daniele Facendola <raf.dan.fac@gmail.com>
2015-07-02 23:16:19 +0200

    IComputation has been refactored. Adding support to input and output resources.

--- GILib/include/gpgpu.h
+++ GILib/include/gpgpu.h
@@ -6,30 +6,15 @@
 #pragma once
 
 #include <typeindex>
-#include <type_traits>
 
-#include "object.h"
 #include "resources.h"
-#include "enums.h"
+#include "tag.h"
+#include "object.h"
 
-#include "gilib.h"
-#include "fnv1.h"
-
-#include "dx11/dx11shader.h"
+#include "texture.h"
 
 namespace gi_lib{
 
-	ENUM_FLAGS(GPUAccess, char){
-
-		Read = (1 << 0),				///< \brief Grants read-only permission.
-		Write = (1 << 1),				///< \brief Grants write permissions.
-
-		Random = Read | Write,			///< \brief Grants read-and-write permissions.
-
-	};
-
-	template <typename TResource, GPUAccess access, typename>
-	class IComputationArgument;
 
 	/// \brief Base interface for GPU computations.
 	/// \author Raffaele D. Facendola
@@ -53,100 +38,91 @@ namespace gi_lib{
 		/// \brief Virtual destructor.
 		virtual ~IComputation(){};
 		
-		/// \brief Get a computation argument by type and access permissions.
-		/// \tparam TArgument Type of the argument to get.
-		/// \tparam access Type of access required by the GPU.
-		/// \param name Name of the argument to get.
-		/// \return Returns a pointer to the argument setter if such argument exists, returns nullptr otherwise.
-		/// \remarks The method fails if the specified type and/or access permissions are wrong, even if the argument exists.
-		template <typename TArgument, GPUAccess access = GPUAccess::Read>
-		ObjectPtr<IComputationArgument<TArgument, access, void>> GetArgument(const string& name);
-				
+		/// \brief Set a texture resource as an input for the current computation.
+		/// The GPU may only read from the specified texture.
+		/// \param tag Tag of the input texture to set.
+		/// \param texture_2D Pointer to the 2D texture to bind.
+		/// \return Returns true if the resource was set successfully, returns false otherwise.
+		virtual bool SetInput(const Tag& tag, ObjectPtr<ITexture2D> texture_2D) = 0;
+
+		/// \brief Set a structure resource as an input for the current computation.
+		/// The GPU may only read from the specified structure.
+		/// \tparam TType Concrete type of the structure.
+		/// \param tag Tag of the input structure to set.
+		/// \param structured_buffer Pointer to the structured buffer to bind.
+		/// \return Returns true if the resource was set successfully, returns false otherwise.
+		template <typename TType>
+		bool SetInput(const Tag& tag, ObjectPtr<IStructuredBuffer<TType>> structured_buffer);
+
+		/// \brief Set an array resource as an input for the current computation.
+		/// The GPU may only read from the specified array.
+		/// \tparam TElement Type of the array's elements.
+		/// \param tag Tag of the input array to set.
+		/// \param structured_array Pointer to the structured array to bind.
+		/// \return Returns true if the resource was set successfully, returns false otherwise.
+		template <typename TElement>
+		bool SetInput(const Tag& tag, ObjectPtr<IStructuredArray<TElement>> structured_array);
+
+		/// \brief Set a texture resource as an input/output for the current computation.
+		/// The GPU has both read and write permissions.
+		/// \param tag Tag of the input/output texture to set.
+		/// \param gp_texture_2D Pointer to the general-purpose 2D texture to bind.
+		/// \return Returns true if the resource was set successfully, returns false otherwise.
+		virtual bool SetOutput(const Tag& tag, ObjectPtr<IGPTexture2D> gp_texture_2D) = 0;
+
 		/// \brief Execute the computation on the GPU.
 		/// \param x Threads to dispatch along the X-axis.
 		/// \param y Threads to dispatch along the Y-axis.
 		/// \param z Threads to dispatch along the Z-axis.
 		/// \remarks The total amount of dispatched threads is x*y*z.
-		virtual void Dispatch(unsigned int x, unsigned int y, unsigned int z) = 0;
+		virtual bool Dispatch(unsigned int x, unsigned int y, unsigned int z) = 0;
 
 	private:
 
-		/// \brief Get a pointer to a computation resource.
-		/// \param name Name of the resource to get.
-		/// \param resource_type Type of the resource to get.
-		/// \param access Type of access required by the computation.
-		/// \remarks The returned object's type must be compatible with IComputationResource<resource_type, access>.
-		virtual ObjectPtr<Object> GetArgument(const string& name, const std::type_index& argument_type, GPUAccess access) = 0;
+		/// \brief Set a structure resource as an input for the current computation.
+		/// The GPU may only read from the specified structure.
+		/// \param tag Tag of the input structure to set.
+		/// \param structured_buffer Pointer to the structured buffer to bind.
+		/// \param type Concrete type of the buffer.
+		/// \return Returns true if the resource was set successfully, returns false otherwise.
+		virtual bool SetInput(const Tag& tag, ObjectPtr<IStructuredBuffer> structured_buffer, type_index type) = 0;
+
+		/// \brief Set an array resource as an input for the current computation.
+		/// The GPU may only read from the specified array.
+		/// \param tag Tag of the input array to set.
+		/// \param structured_array Pointer to the structured array to bind.
+		/// \param type Concrete type of the array elements.
+		/// \return Returns true if the resource was set successfully, returns false otherwise.
+		virtual bool SetInput(const Tag& tag, ObjectPtr<IStructuredArray> structured_array, type_index type) = 0;
 
 	};
+	
+}
 
-	/// \brief Base interface for computation arguments.
-	/// This class is used to set the value of a specific computation.
-	/// \tparam TArgument Type of the argument.
-	/// \tparam access Type of access required by the GPU.
-	template <typename TArgument, GPUAccess access, typename>
-	class IComputationArgument : public Object{
+////////////////////////////// ICOMPUTATION ////////////////////////////////////////////////
 
-	public:
+template <typename TType>
+inline bool SetInput(const Tag& tag, ObjectPtr<IStructuredBuffer<TType>> structured_buffer){
 
-		/// \brief Virtual destructor.
-		virtual ~IComputationArgument(){}
+	return SetInput(tag, 
+					structured_buffer,
+					type_index(typeid(TType)));
 
-		/// \brief Set a new value for a computation argument.
-		/// \param value Value to set.
-		virtual void Set(const TArgument& value) = 0;
+}
 
-	};
+template <typename TElement>
+inline bool SetInput(const Tag& tag, ObjectPtr<IStructuredArray<TElement>> structured_array){
 
-	/// \brief Computation argument specialization for resources (textures, buffers, ...).
-	template <typename TArgument, GPUAccess access>
-	class IComputationArgument < TArgument, access, typename std::enable_if<std::is_base_of<IResource, TArgument>::value>::type > : public Object{
+	return SetInput(tag, 
+					structured_array,
+					type_index(typeid(TElement)));
 
-	public:
+}
 
-		using ResourceView = IResourceView;
+////////////////////////////// MATERIAL :: COMPILE FROM FILE ///////////////////////////////
 
-		/// \brief Virtual destructor.
-		virtual ~IComputationArgument(){}
+inline size_t gi_lib::IComputation::CompileFromFile::GetCacheKey() const{
 
-		/// \brief Bind a new resource as a computation argument.
-		/// \param resource_view Resource to bind.
-		virtual void Set(ObjectPtr<ResourceView> resource_view) = 0;
-
-	};
-
-	/// \brief Computation argument specialization for scalars (integers, floats, ...).
-	template <typename TArgument, GPUAccess access>
-	class IComputationArgument < TArgument, access, typename std::enable_if<std::is_arithmetic<TArgument>::value>::type > : public Object{
-
-	public:
-
-		/// \brief Virtual destructor.
-		virtual ~IComputationArgument(){}
-
-		/// \brief Set a new value for a computation argument.
-		/// \param value Value to set.
-		virtual void Set(TArgument value) = 0;
-
-	};
-
-	/////////////////////////////////// ICOMPUTATION ///////////////////////////////////
-
-	template <typename TArgument, GPUAccess access>
-	inline ObjectPtr<IComputationArgument<TArgument, access, void>> IComputation::GetArgument(const string& name){
-
-		return GetArgument(name,
-						   type_index(typeid(TArgument)),
-						   access);
-
-	}
-
-	////////////////////////////// MATERIAL :: COMPILE FROM FILE ///////////////////////////////
-
-	inline size_t IComputation::CompileFromFile::GetCacheKey() const{
-
-		return ::hash::fnv_1{}(to_string(file_name));
-
-	}
+	return gi_lib::Tag(file_name);
 
 }
