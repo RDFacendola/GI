3011d48a4c0609599753ca324d056e7ebe5b4f8c
Raffaele Daniele Facendola <raf.dan.fac@gmail.com>
2015-07-02 23:14:58 +0200

    IHardwareBuffer, IStructuredBuffer, IStructuredArray, StructuredBuffer and StructuredArray have been added. Dynamic buffer has been removed.

--- GILib/include/buffer.h
+++ GILib/include/buffer.h
@@ -8,71 +8,112 @@
 #include "resources.h"
 
 namespace gi_lib{
-
-	/// \brief Represents a strongly typed hardware array.
-	/// The array can be written by the CPU and read by the GPU.
-	/// The size of the array does not change.
-	/// \author Raffaele D. Facendola
-	class IDynamicBuffer : public IResource{
+	
+	/// \brief Represents a low-level buffer stored in GPU-memory.
+	/// The buffer can be written by the CPU and read by the GPU.
+	/// \author Raffaele D. Facendola.
+	class IHardwareBuffer : public IResource{
 
 	public:
 
-		/// \brief Structure used to build an empty structured vector from a description.
-		struct FromDescription{
+		/// \brief Virtual destructor.
+		virtual ~IHardwareBuffer(){}
 
-			NO_CACHE;
+		/// \brief Lock the buffer, mapping it to the system memory.
+		/// \return Returns a pointer to the mapped buffer.
+		/// \remarks The buffer is intended for write-only purposes. Reading from it results in undefined behavior.
+		virtual void* Lock() = 0;
 
-			size_t element_count;			///< \brief Elements inside the vector.
+		/// \brief Unlock the buffer, committing it back to the video memory.
+		/// \remarks This method will invalidate the pointer returned by "Lock". Do not use that pointer after this method.
+		virtual void Unlock() = 0;
+		
+	};
 
-			size_t element_size;			///< \brief Size of each element.
+	/// \brief Represents a low-level buffer that behaves like a strongly-typed structure.
+	/// The buffer can be written by the CPU and read by the GPU.
+	/// \author Raffaele D. Facendola
+	class IStructuredBuffer : public IHardwareBuffer{
 
-		};
+	public:
+
+		/// \brief Abstract destructor.
+		virtual ~IStructuredBuffer() = 0 {}
+				
+	};
+
+	/// \brief Represents a low-level buffer that behaves like a strongly-typed structure.
+	/// The buffer can be written by the CPU and read by the GPU.
+	/// \tparam TType Concrete type of the buffer.
+	/// \author Raffaele D. Facendola
+	template <typename TType>
+	class StructuredBuffer : public IStructuredBuffer{
+
+	public:
+
+		/// \brief Abstract destructor.
+		virtual ~StructuredBuffer() = 0 {}
+
+		/// \brief Access the structure, granting write permission.
+		/// This method will LOCK the buffer. Remember to unlock it afterwards!
+		/// \return Returns a reference to the actual structure.
+		/// \remarks Reading from the returned reference results in undefined behavior.
+		TType& operator*();
+		
+	};
+
+	/// \brief Represents a low-level buffer that behaves like a strongly-typed array of elements.
+	/// This array can be written by a CPU and read by a GPU.
+	/// \author Raffaele D. Facendola
+	class IStructuredArray : public IHardwareBuffer{
+
+	public:
 
 		/// \brief Virtual destructor.
-		virtual ~IDynamicBuffer(){}
+		virtual ~IStructuredArray(){}
 
-		/// \brief Get the element count of this vector.
-		/// \return Returns the element count of this vector.
-		virtual size_t GetElementCount() const = 0;
-
-		/// \brief Get the size of each element in bytes.
-		/// \return Returns the size of each element.
-		virtual size_t GetElementSize() const = 0;
-
-		/// \brief Get the view to this resource.
-		/// Use this view to bind the vector to the graphic pipeline.
-		/// \return Returns a pointer to the resource view.
-		virtual ObjectPtr<IResourceView> GetView() = 0;
-
-		/// \brief Maps the buffer to the system memory, granting CPU access.
-		/// \tparam TElement Type of the elements to map. Make sure to match the type this buffer was created with!.
-		/// \return Returns a pointer to the first element of the array.
-		/// \remarks Be sure to unlock the buffer afterwards: the context will hang trying to access a locked resource.
-		template <typename TElement>
-		TElement* Lock();
-
-		/// \brief Commit a mapped buffer back to the video memory, revoking CPU access.
-		/// \remarks Unlocking the buffer will invalidate the system-memory pointer returned by the Lock method.
-		virtual void Unlock() = 0;
-
-	private:
-
-		/// \brief Maps the buffer to the system memory, granting CPU access.
-		/// The elements already present inside the vector are discarded.
-		/// \return Returns a pointer to the first element of the array.
-		/// \remarks Be sure to unlock the buffer afterwards: the context will hang trying to access a locked resource.
-		virtual void* LockDiscard() = 0;
+		/// \brief Get the number of elements in the array.
+		virtual size_t GetCount() = 0;
 
 	};
 
-	///////////////////////////////////////// STRUCTURED BUFFER /////////////////////////////////////////
-
+	/// \brief Represents a low-level buffer that behaves like a strongly-typed array of elements.
+	/// This array can be written by a CPU and read by a GPU.
+	/// \tparam TElement Type of the elements stored within the array.
+	/// \author Raffaele D. Facendola
 	template <typename TElement>
-	inline TElement* IDynamicBuffer::Lock(){
+	class StructuredArray : public IStructuredArray{
 
-		return static_cast<TElement*>(LockDiscard());
+	public:
 
-	}
+		/// \brief Abstract destructor.
+		virtual ~StructuredArray() = 0 {}
 
+		/// \brief Access an element of the array, granting write permission.
+		/// This method will LOCK the buffer. Remember to unlock it afterwards!
+		/// \param index Index of the element to access.
+		/// \return Returns a reference to the element at the specified location.
+		/// \remarks Reading from the returned reference results in undefined behavior.
+		TElement& operator[](size_t index);
+
+	};
+
+}
+
+/////////////////////////// STRUCTURED BUFFER //////////////////////////
+
+template <typename TType>
+TType& gi_lib::StructuredBuffer::operator *(){
+
+	return *static_cast<TType*>(Lock());
+	
+}
+
+/////////////////////////// STRUCTURED ARRAY //////////////////////////
+
+template <typename TElement*>
+TType& gi_lib::StructuredArray::operator[](size_t index){
+
+	return static_cast<TElement*>(Lock())[index];
 
 }
