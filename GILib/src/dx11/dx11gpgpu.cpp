5a16691f739ebaad2101932eb3f3f52e64085790
Raffaele Daniele Facendola <raf.dan.fac@gmail.com>
2015-09-21 22:42:31 +0200

    DX11Computation has been implemented.

--- GILib/src/dx11/dx11gpgpu.cpp
+++ GILib/src/dx11/dx11gpgpu.cpp
@@ -30,16 +30,39 @@ shader_composite_(std::make_unique<ShaderStateComposite>()){
 
 	}
 
+	// Access thread group size.
+
+	auto& reflection = shader_composite_->GetShaderState(0).GetReflection().compute_shader;	
+
+	group_size_ = Vector3i(reflection.thread_group_x,
+						   reflection.thread_group_y,
+						   reflection.thread_group_z);
+
 }
 
 DX11Computation::~DX11Computation(){}
 
-void DX11Computation::Dispatch(unsigned int x, unsigned int y, unsigned int z)
-{
+void DX11Computation::Dispatch(ID3D11DeviceContext& context, unsigned int x, unsigned int y, unsigned int z){
+
+	// Bind to the context (implies committing pending resources)
+
+	shader_composite_->Bind(context);
+
+	// Dispatch - Approximate by excess the number of thread groups to dispatch per axis.
+
+	unsigned int dispatch_width = (x + group_size_(0) - 1) / group_size_(0);
+	unsigned int dispatch_height = (y + group_size_(1) - 1) / group_size_(1);
+	unsigned int dispatch_depth = (z + group_size_(2) - 1) / group_size_(2);
+
+	context.Dispatch(dispatch_width,
+					 dispatch_height,
+					 dispatch_depth);
+
+	// Unbind from the context
+
+	shader_composite_->Unbind(context);
 	
-	
+	// Yay!
 
 }
-
-
 
