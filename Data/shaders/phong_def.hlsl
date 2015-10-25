85a13299c844ef935f5aacb7dbdb4f7a8247b953
Tiriath <PLNy42WlpH>
2015-10-26 00:33:20 +0100

    Minor refactor of the phong_def.hlsl file.

--- Data/shaders/phong_def.hlsl
+++ Data/shaders/phong_def.hlsl
@@ -8,27 +8,40 @@
 #include "render_def.hlsl"
 #include "light_def.hlsl"
 
-/// \brief Compute the light contribution using the Phong reflection model.
-/// \param surface Surface informations.
-/// \param light Light shining on the surface.
-/// \param camera_position View position.
-float4 ComputePhong(SurfaceData surface, PointLight light, float3 camera_position) {
-	
-	float3 light_direction = normalize(light.position.xyz - surface.position);					// from surface to light
+/// \brief Compute the illumination based on the Phong reflectance model.
+/// \param light_direction Direction from the surface to the light.
+/// \param camera_position Camera position in world space.
+float3 ComputePhong(float3 light_direction, float3 camera_position, SurfaceData surface) {
+
 	float3 view_direction = normalize(camera_position - surface.position);						// from surface to camera
 	float3 reflection_direction = reflect(-light_direction, surface.normal);					// from surface away
-	
+
 	float4 diffuse = surface.albedo * saturate(dot(light_direction, surface.normal));			// Diffuse contribution
 
 	float4 specular = pow(saturate(dot(reflection_direction, view_direction)),					// Specular contribution
 						  surface.shininess);
 
+	float4 color = (diffuse + specular);
+
+	return color.rgb;
+
+}
+
+/// \brief Compute the light contribution using the Phong reflection model.
+/// \param surface Surface informations.
+/// \param light Light shining on the surface.
+/// \param camera_position View position.
+float4 ComputePhong(SurfaceData surface, PointLight light, float3 camera_position) {
+
+	float3 color = ComputePhong(normalize(light.position.xyz - surface.position),
+								camera_position,
+								surface);
+
 	float attenuation = GetAttenuation(light, surface.position);								// [0;1]
 
-	float4 color = (diffuse + specular) * light.color * attenuation;
+	return float4(color.rgb * light.color * attenuation,										// Attenuate light contribution
+				  surface.albedo.a);															// Restore surface's alpha
 
-	return float4(color.rgb, surface.albedo.a);		// Restore surface's alpha
-	
 }
 
 /// \brief Compute the light contribution using the Phong reflection model.
@@ -37,19 +50,12 @@ float4 ComputePhong(SurfaceData surface, PointLight light, float3 camera_positio
 /// \param camera_position View position.
 float4 ComputePhong(SurfaceData surface, DirectionalLight light, float3 camera_position) {
 
-	float3 light_direction = -light.direction.xyz;												// from surface to light
-	float3 view_direction = normalize(camera_position - surface.position);						// from surface to camera
-	float3 reflection_direction = reflect(-light_direction, surface.normal);					// from surface away
+	float3 color = ComputePhong(-light.direction.xyz,
+								camera_position,
+								surface);
 
-	float4 diffuse = surface.albedo * saturate(dot(light_direction, surface.normal));			// Diffuse contribution
-
-	
-	float4 specular = pow(saturate(dot(reflection_direction, view_direction)),					// Specular contribution
-						  surface.shininess);
-
-	float4 color = (diffuse + specular) * light.color;
-
-	return float4(color.rgb, surface.albedo.a);		// Restore surface's alpha
+	return float4(color.rgb * light.color, 
+				  surface.albedo.a);						// Restore surface's alpha
 
 }
 
