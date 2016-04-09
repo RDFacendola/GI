619f96301bae0096eca3ebe305690cdf3b790239
Raffaele Daniele Facendola <raffaele.facendola@live.com>
2016-04-09 02:20:33 +0200

    Spherical harmonics drawer now applies a tonemap operator to avoid "burning" the LDR buffer used to draw debug infos.

--- Data/shaders/voxel/sh_outline.hlsl
+++ Data/shaders/voxel/sh_outline.hlsl
@@ -1,5 +1,6 @@
 
 #include "voxel_def.hlsl"
+#include "..\common\color.hlsl"
 
 StructuredBuffer<VoxelInfo> gVoxelAppendBuffer;				// Append buffer containing the list of voxels in the current frame. (Read Only)
 
@@ -34,11 +35,17 @@ VSOut VSMain(VSIn input){
 	VSOut output;
 
 	output.color = SampleVoxelColor(gVoxelSH, voxel_info, normalize(input.position.xyz));		// Sampled color of the SH
-	
+
+	// The debug draw is applied after the tonemap. Apply Reinhard here
+
+	output.color.rgb = output.color.rgb * rcp(1.f + output.color.rgb);
+
+	output.color.rgb = pow(output.color.rgb, 1.f / 2.2f);
+
 	// Deformation of the SH mesh
 
-	float magnitude = saturate(max(max(output.color.r, output.color.g), output.color.b));
-
+	float magnitude = Luminance(output.color.rgb);
+	
 	float3 position = (input.position.xyz * voxel_info.size * magnitude) + voxel_info.center;
 	
 	output.position_ps = mul(gViewProjection, float4(position, 1));
